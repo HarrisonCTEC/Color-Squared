5d895a79f7b23f49edc0b4984e39444d463b100b
HarrisonCTEC <harrison.ctec@gmail.com>
2014-11-18 09:08:14 -0700

    Start button goes to game screen

--- src/ctec/color_squared/title_screen.java
+++ src/ctec/color_squared/title_screen.java
@@ -1,16 +1,40 @@
 package ctec.color_squared;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class title_screen extends Activity
 {
+	private Button startButton;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        startButton = (Button) findViewById(R.id.startButton);
+        
         getActionBar().setTitle(R.string.app_name_fancy);
+        
+    }
+    
+    private void setupListeners()
+    {
+    	startButton.setOnClickListener(new View.OnClickListener()
+		{
+			
+			@Override
+			public void onClick(View v)
+			{
+				Intent transferIntent = new Intent(v.getContext(), GameScreen.class);
+				startActivityForResult(transferIntent, 0);
+				
+			}
+		});
     }
 }
