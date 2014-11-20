8c10692bd92f4d55cc54f3a115ef874e0deb2cba
Mark Shamy <shamy.mark@gmail.com>
2014-11-20 00:26:06 -0700

    (DISASTER PREVENTION COMMIT)

--- src/ctec/color_squared/GameScreen.java
+++ src/ctec/color_squared/GameScreen.java
@@ -14,16 +14,32 @@ public class GameScreen extends Activity
 	private List<Color> playerColors;
 	private int playerProgress;
 	private Tiles gameTile;
+	private Color pink, magenta, red, yellow, orange, green, grue, purple;
+	private Color[] gridPattern = new Color[8];
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_screen);
+        
+        pink = Color.parseColor("#E0A0C0");
+        magenta = Color.parseColor("#E04080");
+        red = Color.parseColor("#E04040");
+        yellow = Color.parseColor("#C08000");
+        orange = Color.parseColor("#E06040");
+        green = Color.parseColor("#C0E000");
+        grue = Color.parseColor("#E6EA080");
+        blue = Color.parseColor("#E00A0C0");
+        purple = Color.parseColor("#6080C0");
+     
 
         playerColors = new ArrayList<Color>();
+        levelGenerator();
         playerProgress = 0;
+        displayPlayerColors();
         resetGrid();
+        displayGrid();
 
     }
 	
@@ -32,58 +48,25 @@ public class GameScreen extends Activity
 		gameTile = new Tiles();
 	}
 
-	private void randomPicker()
+	private void levelGenerator()
 	{
 		int randomNumber = (int)Math.ceil(Math.random()*9);
 		int counter = 0;
 		while(counter != 6)
 		{
-//			if(randomNumber == 1)
-//			{
-//				playerColors.add(PINK);
-//			}
-//			else if(randomNumber == 2)
-//			{
-//				playerColors.add(MAGENTA);
-//			}
-//			else if(randomNumber == 3)
-//			{
-//				playerColors.add();
-//			}
-//			else if(randomNumber == 4)
-//			{
-//				playerColors.add(YELLOW);
-//			}
-//			else if(randomNumber == 5)
-//			{
-//				playerColors.add(ORANGE);
-//			}
-//			else if(randomNumber == 6)
-//			{
-//				playerColors.add(GREEN);
-//			}
-//			else if(randomNumber == 7)
-//			{
-//				playerColors.add(BLUE);
-//			}
-//			else if(randomNumber == 8)
-//			{
-//				playerColors.add(GRUE);
-//			}
-//			else if(randomNumber == 9)
-//			{
-//				playerColors.add(PURPLE;
-//			}
-			counter++;
+			playerColors.add(gameTile.randomColor());
+			counter++());
 		}
 	}
 
-	private int updateGrid()
+	private updateGrid(Color currentColor, int playerProgress)
 	{
 		int specialTile = (int) Math.ceil(Math.random() * 9);
 		Color currentColor = (Color) playerColors.get(playerProgress);
 		resetGrid();
-        gameTile.setHotButton(specialTile);
-        return specialTile;
+        	gameTile.setHotButton(specialTile);
+        	gridPattern = gameTile.fillGameGrid(currentColor);
 	}
+
+	private int calculateScore()
 }
