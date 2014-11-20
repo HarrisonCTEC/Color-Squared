15894c631ccc4a374a094cfab6b0b0b481bacd7e
Mark Shamy <shamy.mark@gmail.com>
2014-11-19 23:28:24 -0700

    Update Tiles.java

--- src/ctec/color_squared/Tiles.java
+++ src/ctec/color_squared/Tiles.java
@@ -13,6 +13,20 @@ public class Tiles
 	private Color sevenColor;
 	private Color eightColor;
 	private Color nineColor;
+	private Color[] gridPattern = new Color[8];
+	private boolean[] usedColors = new boolean[8];
+	private Color pink, magenta, red, yellow, orange, green, grue, blue, purple;
+
+	pink = Color.parseColor("#E0A0C0");
+        magenta = Color.parseColor("#E04080");
+        red = Color.parseColor("#E04040");
+        yellow = Color.parseColor("#C08000");
+        orange = Color.parseColor("#E06040");
+        green = Color.parseColor("#C0E000");
+        grue = Color.parseColor("#E6EA080");
+        blue = Color.parseColor("#E00A0C0");
+        purple = Color.parseColor("#6080C0");
+	
 
     	/**
      	* 1-9 represents tiles, 0 represents unset
@@ -33,16 +47,134 @@ public class Tiles
 
         this.hotButton = 0;
 	}
+ 
+ 	public Color randomColor()
+ 	{
+ 		int randomNumber = (int)Math.ceil(Math.random()*9);
+ 		Color returnValue = null;
+ 		if(randomNumber ==)
+ 		{
+ 			
+ 		}
+ 		else if(randomNumber==)
+ 		{
+ 			
+ 		}
+ 		else if(randomNumber==)
+ 		{
+ 			
+ 		}
+ 		else if(randomNumber==)
+ 		{
+ 			
+ 		}
+ 		else if(randomNumber==)
+ 		{
+ 			
+ 		}
+ 		else if(randomNumber==)
+ 		{
+ 			
+ 		}
+ 		else if(randomNumber==)
+ 		{
+ 			
+ 		}
+ 	}
+ 
+ 	private void setUsedColors(Color currentColor)
+ 		if(cuurentColor == pink)
+ 		{
+ 			usedColors[0] = True
+ 		}
+ 		else if(currentColor == magenta)
+ 		{
+ 			usedColors[1] = True	
+ 		}
+ 		else if(currentColor == red)
+ 		{
+ 			usedColors[2] = True	
+ 		}
+ 		else if(currentColor == yellow)
+ 		{
+ 			usedColors[3] = True	
+ 		}
+ 		else if(currentColor == orange)
+ 		{
+ 			usedColors[4] = True
+ 		}
+ 		else if(currentColor == green)
+ 		{
+ 			usedColors[5] = True	
+ 		}
+ 		else if(currentColor == grue)
+ 		{
+ 			usedColors[6] = True	
+ 		}
+ 		else if(currentColor == blue)
+ 		{
+ 			usedColors[7] = True	
+ 		}
+ 		else if(currentColor == purple)
+ 		{
+ 			usedColors[8] = True	
+ 		}
+ 	private void setHotButtonColorAndNumber(int hotButton, Color currentColor)
+ 	{
+ 		if(hotButton == 1)
+		{
+			gridPattern[hotButton] = currentColor;
+			usedColors[]
+		}
+		else if(hotButton == 2)
+		{
+			gridPattern[hotButton] = currentColor;
+		}
+		else if(hotButton == 3)
+		{
+			gridPattern[hotButton] = currentColor;
+		}
+		else if(hotButton == 4)
+		{
+			gridPattern[hotButton] = currentColor;
+		}
+		else if(hotButton == 5)
+		{
+			gridPattern[hotButton] = currentColor;
+		}
+		else if(hotButton == 6)
+		{
+			gridPattern[hotButton] = currentColor;
+		}
+		else if(hotButton == 7)
+		{
+			gridPattern[hotButton] = currentColor;
+		}
+		else if(hotButton == 8)
+		{
+			gridPattern[hotButton] = currentColor;
+		}
+		else if(hotButton == 9)
+		{
+			gridPattern[hotButton] = currentColor;
+		}
+ 	}
+ 
+	public fillGameGrid(Color currentColor)
+	{
+		setHotButtonColorAndNumber(hotButton, currentColor);
+		if(usedColors[1])
+	}
 
-    public void setHotButton(int hotButton)
-    {
-        this.hotButton = hotButton;
-    }
+	public void setHotButton(int hotButton)
+	{
+	    this.hotButton = hotButton;
+	}
 
-    public int getHotButton()
-    {
-        return this.hotButton;
-    }
+	public int getHotButton()
+	{
+        	return this.hotButton;
+    	}
 
 	public void setOneColor(Color oneColor)
 	{
